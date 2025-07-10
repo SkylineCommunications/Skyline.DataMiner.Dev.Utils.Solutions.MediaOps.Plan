5d78e31a9a3b763edcc7fe8329d85d8ad6eda5ba
Jens Vandewalle <jens.vandewalle@skyline.be>
2025-07-10 12:48:31 +0200

    Added core caches

---
+++ Common/Storage/Core/Caches/DataMinerSystemCache.cs
@@ -0,0 +1,116 @@
+﻿namespace Skyline.DataMiner.MediaOps.Plan.Storage.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Skyline.DataMiner.Core.DataMinerSystem.Common;
+
+    internal class DataMinerSystemCache
+    {
+        private readonly IDms dms;
+
+        private readonly Dictionary<DmsElementId, IDmsElement> elementsById = [];
+
+        private readonly Dictionary<DmsServiceId, IDmsService> servicesById = [];
+
+        public DataMinerSystemCache(IDms dms)
+        {
+            this.dms = dms ?? throw new ArgumentNullException(nameof(dms));
+        }
+
+        public IReadOnlyDictionary<DmsElementId, IDmsElement> GetElements(IEnumerable<DmsElementId> elementIds, bool forceGet = false)
+        {
+            if (elementIds == null)
+            {
+                throw new ArgumentNullException(nameof(elementIds));
+            }
+
+            var result = new Dictionary<DmsElementId, IDmsElement>();
+            var idsToRetrieve = new List<DmsElementId>();
+
+            if (forceGet)
+            {
+                idsToRetrieve.AddRange(elementIds.Distinct());
+            }
+            else
+            {
+                foreach (var elementId in elementIds.Distinct())
+                {
+                    if (elementsById.TryGetValue(elementId, out var element))
+                    {
+                        result[elementId] = element;
+                    }
+                    else
+                    {
+                        idsToRetrieve.Add(elementId);
+                    }
+                }
+            }
+
+            if (idsToRetrieve.Count > 0)
+            {
+                foreach (var elementId in idsToRetrieve)
+                {
+                    if (!dms.ElementExists(elementId))
+                    {
+                        continue;
+                    }
+
+                    var element = dms.GetElement(elementId);
+                    result[element.DmsElementId] = element;
+                    elementsById[element.DmsElementId] = element;
+                }
+            }
+
+            return result;
+        }
+
+        public IReadOnlyDictionary<DmsServiceId, IDmsService> GetServices(IEnumerable<DmsServiceId> serviceIds, bool forceGet = false)
+        {
+            if (serviceIds == null)
+            {
+                throw new ArgumentNullException(nameof(serviceIds));
+            }
+
+            var result = new Dictionary<DmsServiceId, IDmsService>();
+            var idsToRetrieve = new List<DmsServiceId>();
+
+            if (forceGet)
+            {
+                idsToRetrieve.AddRange(serviceIds.Distinct());
+            }
+            else
+            {
+                foreach (var serviceId in serviceIds.Distinct())
+                {
+                    if (servicesById.TryGetValue(serviceId, out var service))
+                    {
+                        result[serviceId] = service;
+                    }
+                    else
+                    {
+                        idsToRetrieve.Add(serviceId);
+                    }
+                }
+            }
+
+            if (idsToRetrieve.Count > 0)
+            {
+                foreach (var serviceId in idsToRetrieve)
+                {
+                    if (!dms.ServiceExists(serviceId))
+                    {
+                        continue;
+                    }
+
+                    var service = dms.GetService(serviceId);
+                    result[service.DmsServiceId] = service;
+                    servicesById[service.DmsServiceId] = service;
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- Common/Storage/Core/Helpers/CoreHelpers.cs
+++ Common/Storage/Core/Helpers/CoreHelpers.cs
@@ -2,6 +2,7 @@
 {
     using System;
 
+    using Skyline.DataMiner.Core.DataMinerSystem.Common;
     using Skyline.DataMiner.Net;
     using Skyline.DataMiner.Net.Messages;
     using Skyline.DataMiner.Net.Profiles;
@@ -12,6 +13,10 @@
 
         private readonly Lazy<ResourceManagerHelper> lazyResourceManagerHelper;
         private readonly Lazy<ProfileHelper> lazyProfileHelper;
+        private readonly Lazy<ProtocolFunctionHelper> lazyProtocolFunctionHelper;
+        private readonly Lazy<ProtocolFunctionHelperCache> lazyProtocolFunctionHelperCache;
+        private readonly Lazy<IDms> lazyDms;
+        private readonly Lazy<DataMinerSystemCache> lazyDataMinerSystemCache;
 
         public CoreHelpers(IConnection connection)
         {
@@ -19,10 +24,22 @@
 
             lazyResourceManagerHelper = new Lazy<ResourceManagerHelper>(() => new ResourceManagerHelper(connection.HandleSingleResponseMessage));
             lazyProfileHelper = new Lazy<ProfileHelper>(() => new ProfileHelper(connection.HandleMessages));
+            lazyProtocolFunctionHelper = new Lazy<ProtocolFunctionHelper>(() => new ProtocolFunctionHelper(connection.HandleMessages));
+            lazyProtocolFunctionHelperCache = new Lazy<ProtocolFunctionHelperCache>(() => new ProtocolFunctionHelperCache(ProtocolFunctionHelper));
+            lazyDms = new Lazy<IDms>(() => connection.GetDms());
+            lazyDataMinerSystemCache = new Lazy<DataMinerSystemCache>(() => new DataMinerSystemCache(Dms));
         }
 
         public ResourceManagerHelper ResourceManagerHelper => lazyResourceManagerHelper.Value;
 
         public ProfileHelper ProfileHelper => lazyProfileHelper.Value;
+
+        public ProtocolFunctionHelper ProtocolFunctionHelper => lazyProtocolFunctionHelper.Value;
+
+        public ProtocolFunctionHelperCache ProtocolFunctionHelperCache => lazyProtocolFunctionHelperCache.Value;
+
+        public IDms Dms => lazyDms.Value;
+
+        public DataMinerSystemCache DmsCache => lazyDataMinerSystemCache.Value;
     }
 }
