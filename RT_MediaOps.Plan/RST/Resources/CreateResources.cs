80d6704dfef0a3ddf93537813ef48a648721139e
SKYLINE2\MichielSA <michiel.saelen@skyline.be>
2025-09-17 15:45:56 +0200

    Change test to full CRUD test

--- RT_MediaOps.Plan/RST/Resources/CreateResources.cs
+++ RT_MediaOps.Plan/RST/Resources/CreateResources.cs
@@ -1,22 +1,24 @@
 ﻿namespace RT_MediaOps.Plan.RST.Resources
 {
     using System;
+    using System.Data;
+    using System.Xml.Linq;
 
     using RT_MediaOps.Plan.RegressionTests;
 
     [TestClass]
     [TestCategory("IntegrationTest")]
-    public sealed class CreateResourceTests : IDisposable
+    public sealed class UnmanagedResourcesTests : IDisposable
     {
         private readonly IntegrationTestContext testContext;
 
-        public CreateResourceTests()
+        public UnmanagedResourcesTests()
         {
             testContext = new IntegrationTestContext();
         }
 
         [TestMethod]
-        public void CreateUnmanagedResource()
+        public void UnmanagedResourceCrud()
         {
             string name = TestHelper.GetRandomName("UnmanagedResource_");
             var id = Guid.NewGuid();
@@ -29,15 +31,36 @@
             var returnedId = testContext.Api.Resources.Create(unmanagedResource);
             Assert.AreEqual(id, returnedId);
             var returnedResource = testContext.Api.Resources.Read(returnedId);
-            Assert.AreEqual(name, returnedResource.Name);
-            Assert.AreEqual(5, returnedResource.Concurrency);
-            Assert.IsTrue(returnedResource.IsFavorite);
-            Assert.AreEqual(Skyline.DataMiner.MediaOps.Plan.API.ResourceState.Draft, returnedResource.State);
+            ValidateUnmanagedResource(id, name, 5, true, Skyline.DataMiner.MediaOps.Plan.API.ResourceState.Draft, returnedResource);
+            testContext.Api.Resources.MoveTo(id, Skyline.DataMiner.MediaOps.Plan.API.ResourceState.Complete);
+            returnedResource = testContext.Api.Resources.Read(returnedId);
+            ValidateUnmanagedResource(id, name, 5, true, Skyline.DataMiner.MediaOps.Plan.API.ResourceState.Complete, returnedResource);
+            returnedResource.Concurrency = 10;
+            returnedResource.IsFavorite = false;
+            returnedResource.Name = name + "_updated";
+            testContext.Api.Resources.Update(returnedResource);
+            ValidateUnmanagedResource(id, name + "_updated", 10, false, Skyline.DataMiner.MediaOps.Plan.API.ResourceState.Complete, returnedResource);
+            testContext.Api.Resources.Delete(returnedResource);
         }
 
         public void Dispose()
         {
             testContext.Dispose();
+        }
+
+        private static void ValidateUnmanagedResource(
+            Guid expectedId,
+            string expectedName,
+            int expectedConcurrency,
+            bool expectedIsFavorite,
+            Skyline.DataMiner.MediaOps.Plan.API.ResourceState expectedState,
+            Skyline.DataMiner.MediaOps.Plan.API.Resource resource)
+        {
+            Assert.AreEqual(expectedId, resource.Id);
+            Assert.AreEqual(expectedName, resource.Name);
+            Assert.AreEqual(expectedConcurrency, resource.Concurrency);
+            Assert.AreEqual(expectedIsFavorite, resource.IsFavorite);
+            Assert.AreEqual(expectedState, resource.State);
         }
     }
 }
