73a0ea537f7345be4414e52fceb156fb2d05a3c6
Jens Vandewalle <jens.vandewalle@skyline.be>
2025-11-28 13:17:44 +0100

    Added IDomTemporaryCache

---
+++ DevPack.Tests/RST/Resources/StorageTests.cs
@@ -0,0 +1,309 @@
+﻿namespace RT_MediaOps.Plan.RST.Resources
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Skyline.DataMiner.Solutions.MediaOps.Plan.Storage.DOM;
+    using Skyline.DataMiner.Solutions.MediaOps.Plan.Storage.DOM.SlcResource_Studio;
+
+    [TestClass]
+    public sealed class StorageTests
+    {
+        [TestMethod]
+        public void SetCache_HappyPath()
+        {
+            var resourceInstance = new ResourceInstance();
+
+            var resourcePool1 = new ResourcepoolInstance();
+            var resourcePool2 = new ResourcepoolInstance();
+            resourceInstance.SetCache<ResourcepoolInstance>([resourcePool1, resourcePool2]);
+
+            var propertyConfiguration1 = new ResourcepropertyInstance();
+            var propertyConfiguration2 = new ResourcepropertyInstance();
+            resourceInstance.SetCache<ResourcepropertyInstance>([propertyConfiguration1, propertyConfiguration2]);
+
+            var cachedResourcePools = resourceInstance.GetFromCache<ResourcepoolInstance>().ToList();
+            Assert.IsNotNull(cachedResourcePools);
+            Assert.AreEqual(2, cachedResourcePools.OfType<ResourcepoolInstance>().Count());
+            Assert.IsTrue(cachedResourcePools.Exists(x => x.ID.Id == resourcePool1.ID.Id));
+            Assert.IsTrue(cachedResourcePools.Exists(x => x.ID.Id == resourcePool2.ID.Id));
+
+            var cachedPropertyConfigurations = resourceInstance.GetFromCache<ResourcepropertyInstance>().ToList();
+            Assert.IsNotNull(cachedPropertyConfigurations);
+            Assert.AreEqual(2, cachedPropertyConfigurations.OfType<ResourcepropertyInstance>().Count());
+            Assert.IsTrue(cachedPropertyConfigurations.Exists(x => x.ID.Id == propertyConfiguration1.ID.Id));
+            Assert.IsTrue(cachedPropertyConfigurations.Exists(x => x.ID.Id == propertyConfiguration2.ID.Id));
+        }
+
+        [TestMethod]
+        public void SetCache_Update()
+        {
+            var resourceInstance = new ResourceInstance();
+
+            var resourcePool1 = new ResourcepoolInstance()
+            {
+                ResourcePoolInfo = new ResourcePoolInfoSection
+                {
+                    Name = "Pool 1",
+                },
+            };
+            var resourcePool2 = new ResourcepoolInstance()
+            {
+                ResourcePoolInfo = new ResourcePoolInfoSection
+                {
+                    Name = "Pool 2",
+                },
+            };
+            resourceInstance.SetCache<ResourcepoolInstance>([resourcePool1, resourcePool2]);
+
+            var cachedResourcePools = resourceInstance.GetFromCache<ResourcepoolInstance>().ToList();
+            Assert.IsNotNull(cachedResourcePools);
+            Assert.AreEqual(2, cachedResourcePools.OfType<ResourcepoolInstance>().Count());
+            Assert.IsTrue(cachedResourcePools.Exists(x => x.ID.Id == resourcePool1.ID.Id && x.ResourcePoolInfo.Name == "Pool 1"));
+            Assert.IsTrue(cachedResourcePools.Exists(x => x.ID.Id == resourcePool2.ID.Id && x.ResourcePoolInfo.Name == "Pool 2"));
+
+            resourcePool1.ResourcePoolInfo.Name = "Pool 1_Updated";
+            var resourcePool3 = new ResourcepoolInstance()
+            {
+                ResourcePoolInfo = new ResourcePoolInfoSection
+                {
+                    Name = "Pool 3",
+                },
+            };
+            resourceInstance.SetCache<ResourcepoolInstance>([resourcePool1, resourcePool3]);
+
+            cachedResourcePools = resourceInstance.GetFromCache<ResourcepoolInstance>().ToList();
+            Assert.IsNotNull(cachedResourcePools);
+            Assert.AreEqual(2, cachedResourcePools.OfType<ResourcepoolInstance>().Count());
+            Assert.IsTrue(cachedResourcePools.Exists(x => x.ID.Id == resourcePool1.ID.Id && x.ResourcePoolInfo.Name == "Pool 1_Updated"));
+            Assert.IsTrue(cachedResourcePools.Exists(x => x.ID.Id == resourcePool3.ID.Id && x.ResourcePoolInfo.Name == "Pool 3"));
+        }
+
+        [TestMethod]
+        public void SetCache_NullCollection()
+        {
+            var resourceInstance = new ResourceInstance();
+
+            try
+            {
+                resourceInstance.SetCache<ResourcepoolInstance>(null);
+            }
+            catch (ArgumentNullException)
+            {
+                // Expected
+                return;
+            }
+
+            Assert.Fail("Expected exception was not thrown.");
+        }
+
+        [TestMethod]
+        public void SetCache_EmptyCollection()
+        {
+            var resourceInstance = new ResourceInstance();
+            resourceInstance.SetCache<ResourcepoolInstance>([]);
+        }
+
+        [TestMethod]
+        public void SetCache_CollectionWithNulls()
+        {
+            var resourceInstance = new ResourceInstance();
+
+            var resourcePool1 = new ResourcepoolInstance();
+            var resourcePool2 = new ResourcepoolInstance();
+
+            try
+            {
+                resourceInstance.SetCache<ResourcepoolInstance>([resourcePool1, null, resourcePool2, null]);
+            }
+            catch (ArgumentException)
+            {
+                // Expected
+                return;
+            }
+
+            Assert.Fail("Expected exception was not thrown.");
+        }
+
+        [TestMethod]
+        public void SetCache_MixedCollection()
+        {
+            var resourceInstance = new ResourceInstance();
+
+            var resourcePool1 = new ResourcepoolInstance();
+            var resourcePool2 = new ResourcepoolInstance();
+
+            var propertyConfiguration1 = new ResourcepropertyInstance();
+            var propertyConfiguration2 = new ResourcepropertyInstance();
+
+            try
+            {
+                resourceInstance.SetCache<DomInstanceBase>([resourcePool1, resourcePool2, propertyConfiguration1, propertyConfiguration2]);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Assert.AreEqual("Cannot use DomInstanceBase directly. Use a derived type.", ex.Message);
+                return;
+            }
+
+            Assert.Fail("Expected exception was not thrown.");
+        }
+
+        [TestMethod]
+        public void AddToCache_HappyPath()
+        {
+            var resourceInstance = new ResourceInstance();
+
+            var resourcePool1 = new ResourcepoolInstance();
+            var resourcePool2 = new ResourcepoolInstance();
+            resourceInstance.AddToCache<ResourcepoolInstance>([resourcePool1, resourcePool2]);
+
+            var propertyConfiguration1 = new ResourcepropertyInstance();
+            var propertyConfiguration2 = new ResourcepropertyInstance();
+            resourceInstance.AddToCache<ResourcepropertyInstance>([propertyConfiguration1, propertyConfiguration2]);
+
+            var cachedResourcePools = resourceInstance.GetFromCache<ResourcepoolInstance>().ToList();
+            Assert.IsNotNull(cachedResourcePools);
+            Assert.AreEqual(2, cachedResourcePools.OfType<ResourcepoolInstance>().Count());
+            Assert.IsTrue(cachedResourcePools.Exists(x => x.ID.Id == resourcePool1.ID.Id));
+            Assert.IsTrue(cachedResourcePools.Exists(x => x.ID.Id == resourcePool2.ID.Id));
+
+            var cachedPropertyConfigurations = resourceInstance.GetFromCache<ResourcepropertyInstance>().ToList();
+            Assert.IsNotNull(cachedPropertyConfigurations);
+            Assert.AreEqual(2, cachedPropertyConfigurations.OfType<ResourcepropertyInstance>().Count());
+            Assert.IsTrue(cachedPropertyConfigurations.Exists(x => x.ID.Id == propertyConfiguration1.ID.Id));
+            Assert.IsTrue(cachedPropertyConfigurations.Exists(x => x.ID.Id == propertyConfiguration2.ID.Id));
+        }
+
+        [TestMethod]
+        public void AddToCache_Update()
+        {
+            var resourceInstance = new ResourceInstance();
+
+            var resourcePool1 = new ResourcepoolInstance()
+            {
+                ResourcePoolInfo = new ResourcePoolInfoSection
+                {
+                    Name = "Pool 1",
+                },
+            };
+            var resourcePool2 = new ResourcepoolInstance()
+            {
+                ResourcePoolInfo = new ResourcePoolInfoSection
+                {
+                    Name = "Pool 2",
+                },
+            };
+            resourceInstance.AddToCache<ResourcepoolInstance>([resourcePool1, resourcePool2]);
+
+            var cachedResourcePools = resourceInstance.GetFromCache<ResourcepoolInstance>().ToList();
+            Assert.IsNotNull(cachedResourcePools);
+            Assert.AreEqual(2, cachedResourcePools.OfType<ResourcepoolInstance>().Count());
+            Assert.IsTrue(cachedResourcePools.Exists(x => x.ID.Id == resourcePool1.ID.Id && x.ResourcePoolInfo.Name == "Pool 1"));
+            Assert.IsTrue(cachedResourcePools.Exists(x => x.ID.Id == resourcePool2.ID.Id && x.ResourcePoolInfo.Name == "Pool 2"));
+
+            resourcePool1.ResourcePoolInfo.Name = "Pool 1_Updated";
+            var resourcePool3 = new ResourcepoolInstance()
+            {
+                ResourcePoolInfo = new ResourcePoolInfoSection
+                {
+                    Name = "Pool 3",
+                },
+            };
+            resourceInstance.AddToCache<ResourcepoolInstance>([resourcePool1, resourcePool3]);
+
+            cachedResourcePools = resourceInstance.GetFromCache<ResourcepoolInstance>().ToList();
+            Assert.IsNotNull(cachedResourcePools);
+            Assert.AreEqual(3, cachedResourcePools.OfType<ResourcepoolInstance>().Count());
+            Assert.IsTrue(cachedResourcePools.Exists(x => x.ID.Id == resourcePool1.ID.Id && x.ResourcePoolInfo.Name == "Pool 1_Updated"));
+            Assert.IsTrue(cachedResourcePools.Exists(x => x.ID.Id == resourcePool2.ID.Id && x.ResourcePoolInfo.Name == "Pool 2"));
+            Assert.IsTrue(cachedResourcePools.Exists(x => x.ID.Id == resourcePool3.ID.Id && x.ResourcePoolInfo.Name == "Pool 3"));
+        }
+
+        [TestMethod]
+        public void AddToCache_NullCollection()
+        {
+            var resourceInstance = new ResourceInstance();
+
+            try
+            {
+                resourceInstance.AddToCache<ResourcepoolInstance>(null);
+            }
+            catch (ArgumentNullException)
+            {
+                // Expected
+                return;
+            }
+
+            Assert.Fail("Expected exception was not thrown.");
+        }
+
+        [TestMethod]
+        public void AddToCache_EmptyCollection()
+        {
+            var resourceInstance = new ResourceInstance();
+            resourceInstance.AddToCache<ResourcepoolInstance>([]);
+        }
+
+        [TestMethod]
+        public void AddToCache_CollectionWithNulls()
+        {
+            var resourceInstance = new ResourceInstance();
+
+            var resourcePool1 = new ResourcepoolInstance();
+            var resourcePool2 = new ResourcepoolInstance();
+
+            try
+            {
+                resourceInstance.AddToCache<ResourcepoolInstance>([resourcePool1, null, resourcePool2, null]);
+            }
+            catch (ArgumentException)
+            {
+                // Expected
+                return;
+            }
+
+            Assert.Fail("Expected exception was not thrown.");
+        }
+
+        [TestMethod]
+        public void AddToCache_MixedCollection()
+        {
+            var resourceInstance = new ResourceInstance();
+
+            var resourcePool1 = new ResourcepoolInstance();
+            var resourcePool2 = new ResourcepoolInstance();
+
+            var propertyConfiguration1 = new ResourcepropertyInstance();
+            var propertyConfiguration2 = new ResourcepropertyInstance();
+
+            try
+            {
+                resourceInstance.AddToCache<DomInstanceBase>([resourcePool1, resourcePool2, propertyConfiguration1, propertyConfiguration2]);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Assert.AreEqual("Cannot use DomInstanceBase directly. Use a derived type.", ex.Message);
+                return;
+            }
+
+            Assert.Fail("Expected exception was not thrown.");
+        }
+
+        [TestMethod]
+        public void GetFromCache_NotCachedInstance()
+        {
+            var resourceInstance = new ResourceInstance();
+
+            var resourcePool1 = new ResourcepoolInstance();
+            var resourcePool2 = new ResourcepoolInstance();
+            resourceInstance.SetCache<ResourcepoolInstance>([resourcePool1, resourcePool2]);
+
+            var cachedPropertyConfigurations = resourceInstance.GetFromCache<ResourcepropertyInstance>().ToList();
+            Assert.IsNotNull(cachedPropertyConfigurations);
+        }
+    }
+}
--- Shared/Storage/DOM/Model/Partials/ResourceInstance.cs
+++ Shared/Storage/DOM/Model/Partials/ResourceInstance.cs
@@ -1,10 +1,14 @@
 ﻿namespace Skyline.DataMiner.Solutions.MediaOps.Plan.Storage.DOM.SlcResource_Studio
 {
     using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
     using System.Linq;
 
-    internal partial class ResourceInstance
+    internal partial class ResourceInstance : IDomTemporaryCache
     {
+        private readonly ConcurrentDictionary<Type, List<DomInstanceBase>> cache = new();
+
         protected override void BeforeToInstance()
         {
             ResourceInternalProperties.ApplyChanges();
@@ -22,6 +26,98 @@
 
             foreach (var errorToRemove in errorsToRemove) Errors.Remove(errorToRemove);
             return true;
+        }
+
+        public void SetCache<T>(IEnumerable<T> instances) where T : DomInstanceBase
+        {
+            var type = typeof(T);
+            if (type == typeof(DomInstanceBase))
+            {
+                throw new InvalidOperationException("Cannot use DomInstanceBase directly. Use a derived type.");
+            }
+
+            if (instances == null)
+            {
+                throw new ArgumentNullException(nameof(instances));
+            }
+
+            if (!instances.Any())
+            {
+                return;
+            }
+
+            if (instances.Any(x => x == null))
+            {
+                throw new ArgumentException("instances collection contains null values", nameof(instances));
+            }
+
+            if (!cache.TryGetValue(type, out var cachedInstances))
+            {
+                cachedInstances = new List<DomInstanceBase>();
+                cache.TryAdd(type, cachedInstances);
+            }
+
+            cachedInstances.Clear();
+            cachedInstances.AddRange(instances);
+        }
+
+        public void AddToCache<T>(IEnumerable<T> instances) where T : DomInstanceBase
+        {
+            var type = typeof(T);
+            if (type == typeof(DomInstanceBase))
+            {
+                throw new InvalidOperationException("Cannot use DomInstanceBase directly. Use a derived type.");
+            }
+
+            if (instances == null)
+            {
+                throw new ArgumentNullException(nameof(instances));
+            }
+
+            if (!instances.Any())
+            {
+                return;
+            }
+
+            if (instances.Any(x => x == null))
+            {
+                throw new ArgumentException("instances collection contains null values", nameof(instances));
+            }
+
+            if (!cache.TryGetValue(type, out var cachedInstances))
+            {
+                cachedInstances = new List<DomInstanceBase>(instances);
+                cache.TryAdd(type, cachedInstances);
+
+                return;
+            }
+
+            var cachedInstancesById = cachedInstances.ToDictionary(x => x.ID.Id);
+            foreach (var instance in instances)
+            {
+                if (cachedInstancesById.ContainsKey(instance.ID.Id))
+                {
+                    cachedInstances.Remove(cachedInstancesById[instance.ID.Id]);
+                }
+
+                cachedInstances.Add(instance);
+            }
+        }
+
+        public IEnumerable<T> GetFromCache<T>() where T : DomInstanceBase
+        {
+            var type = typeof(T);
+            if (type == typeof(DomInstanceBase))
+            {
+                throw new InvalidOperationException("Cannot use DomInstanceBase directly. Use a derived type.");
+            }
+
+            if (!cache.TryGetValue(typeof(T), out var cachedInstances))
+            {
+                return Enumerable.Empty<T>();
+            }
+
+            return cachedInstances.OfType<T>();
         }
     }
 }
