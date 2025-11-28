b7157a75a9fda1860f331f178797d93012c024cd
Jens Vandewalle <jens.vandewalle@skyline.be>
2025-11-28 13:17:44 +0100

    Added IDomTemporaryCache

---
+++ Shared/Storage/DOM/Model/IDomTemporaryCache.cs
@@ -0,0 +1,13 @@
+﻿namespace Skyline.DataMiner.Solutions.MediaOps.Plan.Storage.DOM
+{
+    using System.Collections.Generic;
+
+    internal interface IDomTemporaryCache
+    {
+        void SetCache<T>(IEnumerable<T> instances) where T : DomInstanceBase;
+
+        void AddToCache<T>(IEnumerable<T> instances) where T : DomInstanceBase;
+
+        IEnumerable<T> GetFromCache<T>() where T : DomInstanceBase;
+    }
+}
