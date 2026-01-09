bc4b0596746f34c0c6832274365fdfb8596cba9f
SKYLINE2\ThomasRE <thomas.remmery@skyline.be>
2026-01-09 09:33:24 +0100

    Add ILoggerExtensions class.

---
+++ DevPack/Extensions/ILoggerExtensions.cs
@@ -0,0 +1,40 @@
+﻿namespace Skyline.DataMiner.Solutions.MediaOps.Plan.Extensions
+{
+    using System.Runtime.CompilerServices;
+    using Microsoft.Extensions.Logging;
+
+    internal static class ILoggerExtensions
+    {
+        public static void LogTrace(this ILogger logger, object callerInstance, string message, [CallerMemberName] string methodName = "")
+        {
+            if (logger == null)
+                return;
+
+            logger.LogTrace("{0}.{1}|{2}", callerInstance.GetType().Name, methodName, message);
+        }
+
+        public static void LogDebug(this ILogger logger, object callerInstance, string message, [CallerMemberName] string methodName = "")
+        {
+            if (logger == null)
+                return;
+
+            logger.LogDebug("{0}.{1}|{2}", callerInstance.GetType().Name, methodName, message);
+        }
+
+        public static void LogInformation(this ILogger logger, object callerInstance, string message, [CallerMemberName] string methodName = "")
+        {
+            if (logger == null)
+                return;
+
+            logger.LogInformation("{0}.{1}|{2}", callerInstance.GetType().Name, methodName, message);
+        }
+
+        public static void LogError(this ILogger logger, object callerInstance, string message, [CallerMemberName] string methodName = "")
+        {
+            if (logger == null)
+                return;
+
+            logger.LogError("{0}.{1}|{2}", callerInstance.GetType().Name, methodName, message);
+        }
+    }
+}
