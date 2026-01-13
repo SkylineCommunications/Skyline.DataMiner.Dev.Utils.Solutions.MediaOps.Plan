44d801ebd491f94a4fcfee56d57c047a8f27f345
SKYLINE2\ThomasRE <thomas.remmery@skyline.be>
2026-01-13 15:14:19 +0100

    Add SlcWorkflowHelper with basic implementation for Jobs, Workflows and Configurations.

---
+++ DevPack/Storage/DOM/Helpers/SlcWorkflowHelper.cs
@@ -0,0 +1,69 @@
+﻿namespace Skyline.DataMiner.Solutions.MediaOps.Plan.Storage.DOM
+{
+    using System;
+    using System.Collections.Generic;
+    using Skyline.DataMiner.Net;
+    using Skyline.DataMiner.Net.Apps.DataMinerObjectModel;
+    using Skyline.DataMiner.Net.Messages.SLDataGateway;
+    using Skyline.DataMiner.Solutions.MediaOps.Plan.Storage.DOM.SlcWorkflow;
+
+    internal class SlcWorkflowHelper : DomModuleHelperBase
+    {
+        public SlcWorkflowHelper(IConnection connection) : base(SlcWorkflowIds.ModuleId, connection)
+        {
+        }
+
+        public long CountWorkflowInstances(FilterElement<DomInstance> filter)
+        {
+            if (filter == null)
+            {
+                throw new ArgumentNullException(nameof(filter));
+            }
+
+            return DomHelper.DomInstances.Count(filter);
+        }
+
+        public IEnumerable<ConfigurationInstance> GetConfigurations(FilterElement<DomInstance> filter)
+        {
+            if (filter == null)
+            {
+                throw new ArgumentNullException(nameof(filter));
+            }
+            return GetConfigurationIterator(filter);
+        }
+
+        public IEnumerable<JobsInstance> GetJobs(FilterElement<DomInstance> filter)
+        {
+            if (filter == null)
+            {
+                throw new ArgumentNullException(nameof(filter));
+            }
+
+            return GetJobIterator(filter);
+        }
+
+        public IEnumerable<WorkflowsInstance> GetWorkflows(FilterElement<DomInstance> filter)
+        {
+            if (filter == null)
+            {
+                throw new ArgumentNullException(nameof(filter));
+            }
+            return GetWorkflowIterator(filter);
+        }
+
+        private IEnumerable<ConfigurationInstance> GetConfigurationIterator(FilterElement<DomInstance> filter)
+        {
+            return InstanceFactory.ReadAndCreateInstances(DomHelper, filter, instance => new ConfigurationInstance(instance));
+        }
+
+        private IEnumerable<JobsInstance> GetJobIterator(FilterElement<DomInstance> filter)
+        {
+            return InstanceFactory.ReadAndCreateInstances(DomHelper, filter, instance => new JobsInstance(instance));
+        }
+
+        private IEnumerable<WorkflowsInstance> GetWorkflowIterator(FilterElement<DomInstance> filter)
+        {
+            return InstanceFactory.ReadAndCreateInstances(DomHelper, filter, instance => new WorkflowsInstance(instance));
+        }
+    }
+}
