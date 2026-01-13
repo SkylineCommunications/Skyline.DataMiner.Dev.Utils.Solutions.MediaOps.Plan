4a2429d52544d313b6f579a6e8bd134db18cbb39
SKYLINE2\ThomasRE <thomas.remmery@skyline.be>
2026-01-13 17:04:28 +0100

    Added check to verify if Configuration Parameter is not being referenced by a Resource Pool. Started adding checks for Jobs, Recurring Jobs and Workflows.

--- DevPack/API/Handlers/Configurations/CoreConfigurationHandler.cs
+++ DevPack/API/Handlers/Configurations/CoreConfigurationHandler.cs
@@ -7,9 +7,10 @@
 
     using Microsoft.Extensions.Logging;
 
-    using Skyline.DataMiner.Net;
-    using Skyline.DataMiner.Net.AppPackages;
+    using Skyline.DataMiner.Net.Apps.DataMinerObjectModel;
+    using Skyline.DataMiner.Net.Messages.SLDataGateway;
     using Skyline.DataMiner.Solutions.MediaOps.Plan.Exceptions;
+    using Skyline.DataMiner.Solutions.MediaOps.Plan.Storage.DOM.SlcResource_Studio;
 
     internal class CoreConfigurationHandler : ApiObjectValidator
     {
@@ -121,6 +122,7 @@
             }
 
             ValidateExistence(apiConfigurations);
+            ValidateResourcePoolParametersUsage(apiConfigurations.Where(IsValid).ToArray());
 
             var validConfigurations = apiConfigurations.Where(IsValid).ToList();
             var lockResult = planApi.LockManager.LockAndExecute(validConfigurations, DeleteCoreConfigurations);
@@ -358,9 +360,145 @@
             }
         }
 
-        private void ValidateUsage(ICollection<Configuration> apiConfigurations)
+        private void ValidateResourcePoolParametersUsage(ICollection<Configuration> apiConfigurations)
         {
-            planApi.Resources.Read(ResourceExposers.)
+            var resourcePoolConfigurations = GetResourceConfigurationsReferencingParameters(apiConfigurations.Select(x => x.Id).ToHashSet());
+
+            if (!resourcePoolConfigurations.Any())
+                return;
+
+            var resourcePools = GetResourcePoolsReferencingConfiguration(resourcePoolConfigurations);
+
+            foreach (var apiConfiguration in apiConfigurations)
+            {
+                var poolConfigurationsUsingThisConfiguration = resourcePoolConfigurations.Where(x =>
+                x.ProfileParameterValues.Any(y => y.ProfileParameterID == apiConfiguration.Id.ToString()) ||
+                x.OrchestrationEvents.Any(y => y.ScriptExecutionDetails.ProfileParameterValues.Select(x => x.ProfileParameterId).Contains(apiConfiguration.Id))).ToArray();
+
+                if (!poolConfigurationsUsingThisConfiguration.Any())
+                    continue;
+
+                var poolConfigurationIdsUsingThisConfiguration = poolConfigurationsUsingThisConfiguration.Select(x => x.ID.Id).Distinct();
+                var poolsUsingThisConfiguration = resourcePools.Where(x => x.ConfigurationInfo.PoolConfiguration != null && poolConfigurationIdsUsingThisConfiguration.Contains(x.ConfigurationInfo.PoolConfiguration.Value)).ToArray();
+
+                if (!poolsUsingThisConfiguration.Any())
+                    continue;
+
+                ReportError(apiConfiguration.Id, new ConfigurationInUseByResourcePoolsError
+                {
+                    ErrorMessage = $"Configuration '{apiConfiguration.Name}' is in use by Resource Pools.",
+                    Id = apiConfiguration.Id,
+                    ResourcePoolIds = poolsUsingThisConfiguration.Select(x => x.ID.Id).ToArray(),
+                });
+            }
+        }
+
+        private ICollection<ResourcepoolInstance> GetResourcePoolsReferencingConfiguration(ICollection<ConfigurationInstance> resourcePoolConfigurations)
+        {
+            var resourcePoolFilter = DomInstanceExposers.DomDefinitionId.Equal(Storage.DOM.SlcResource_Studio.SlcResource_StudioIds.Definitions.Resourcepool.Id);
+            var distinctResourcePoolConfigurationIds = resourcePoolConfigurations.Select(x => x.ID.Id).Distinct().ToList();
+            resourcePoolFilter.AND(new ORFilterElement<DomInstance>(distinctResourcePoolConfigurationIds.Select(x => DomInstanceExposers.FieldValues.DomInstanceField(Storage.DOM.SlcResource_Studio.SlcResource_StudioIds.Sections.ConfigurationInfo.PoolConfiguration).Equal(x)).ToArray()));
+
+            return planApi.DomHelpers.SlcResourceStudioHelper.GetResourcePools(resourcePoolFilter).ToArray();
+        }
+
+        private ICollection<ConfigurationInstance> GetResourceConfigurationsReferencingParameters(HashSet<Guid> apiParameterIds)
+        {
+            var configurationFilter = DomInstanceExposers.DomDefinitionId.Equal(Storage.DOM.SlcResource_Studio.SlcResource_StudioIds.Definitions.Configuration.Id);
+            configurationFilter.AND(new ORFilterElement<DomInstance>(apiParameterIds.Select(x =>
+                DomInstanceExposers.FieldValues.DomInstanceField(Storage.DOM.SlcResource_Studio.SlcResource_StudioIds.Sections.ProfileParameterValues.ProfileParameterID).Equal(x).OR(
+                DomInstanceExposers.FieldValues.DomInstanceField(Storage.DOM.SlcResource_Studio.SlcResource_StudioIds.Sections.OrchestrationEvents.ScriptInput).Contains(x.ToString())))
+                .ToArray()));
+
+            var possibleResourcePoolConfigurations = planApi.DomHelpers.SlcResourceStudioHelper.GetConfigurations(configurationFilter);
+
+            List<ConfigurationInstance> resourcePoolConfigurations = new List<ConfigurationInstance>();
+            if (!possibleResourcePoolConfigurations.Any())
+                return resourcePoolConfigurations;
+
+            foreach (var possibleResourcePoolConfiguration in possibleResourcePoolConfigurations)
+            {
+                if (possibleResourcePoolConfiguration.ProfileParameterValues.Any(x => Guid.TryParse(x.ProfileParameterID, out Guid profileParameterGuid) && apiParameterIds.Contains(profileParameterGuid)))
+                {
+                    resourcePoolConfigurations.Add(possibleResourcePoolConfiguration);
+                    continue;
+                }
+
+                foreach (var section in possibleResourcePoolConfiguration.OrchestrationEvents)
+                {
+                    if (section.ScriptExecutionDetails?.ProfileParameterValues == null)
+                    {
+                        continue;
+                    }
+
+                    if (!section.ScriptExecutionDetails.ProfileParameterValues.Any(x => apiParameterIds.Contains(x.ProfileParameterId)))
+                    {
+                        continue;
+                    }
+
+                    resourcePoolConfigurations.Add(possibleResourcePoolConfiguration);
+                    break;
+                }
+            }
+
+            return resourcePoolConfigurations;
+        }
+
+        private void ValidateWorkflowUsage(ICollection<Configuration> apiConfigurations)
+        {
+            var workflowConfigurations = GetWorkflowConfigurationsReferencingParameters(apiConfigurations.Select(x => x.Id).ToHashSet());
+
+            if (!workflowConfigurations.Any())
+                return;
+
+            // TODO: check Jobs
+            // TODO: check job nodes
+            // TODO: check workflows
+            // TODO: check workflow nodes
+            // TODO: check recurring jobs
+            // TODO: check recurring job nodes
+        }
+
+        private ICollection<Storage.DOM.SlcWorkflow.ConfigurationInstance> GetWorkflowConfigurationsReferencingParameters(HashSet<Guid> apiParameterIds)
+        {
+            var configurationFilter = DomInstanceExposers.DomDefinitionId.Equal(Storage.DOM.SlcWorkflow.SlcWorkflowIds.Definitions.Configuration.Id);
+            configurationFilter.AND(new ORFilterElement<DomInstance>(apiParameterIds.Select(x =>
+                DomInstanceExposers.FieldValues.DomInstanceField(Storage.DOM.SlcWorkflow.SlcWorkflowIds.Sections.ProfileParameterValues.ProfileParameterID).Equal(x).OR(
+                DomInstanceExposers.FieldValues.DomInstanceField(Storage.DOM.SlcWorkflow.SlcWorkflowIds.Sections.OrchestrationEvents.ScriptInputValues).Contains(x.ToString())))
+                .ToArray()));
+
+            var possibleWorkflowConfigurations = planApi.DomHelpers.SlcWorkflowHelper.GetConfigurations(configurationFilter);
+
+            List<Storage.DOM.SlcWorkflow.ConfigurationInstance> workflowConfigurations = new List<Storage.DOM.SlcWorkflow.ConfigurationInstance>();
+            if (!possibleWorkflowConfigurations.Any())
+                return workflowConfigurations;
+
+            foreach (var possibleWorkflowConfiguration in possibleWorkflowConfigurations)
+            {
+                if (possibleWorkflowConfiguration.ProfileParameterValues.Any(x => Guid.TryParse(x.ProfileParameterID, out Guid profileParameterGuid) && apiParameterIds.Contains(profileParameterGuid)))
+                {
+                    workflowConfigurations.Add(possibleWorkflowConfiguration);
+                    continue;
+                }
+
+                foreach (var section in possibleWorkflowConfiguration.OrchestrationEvents)
+                {
+                    if (section.ScriptExecutionDetails?.ProfileParameterValues == null)
+                    {
+                        continue;
+                    }
+
+                    if (!section.ScriptExecutionDetails.ProfileParameterValues.Any(x => apiParameterIds.Contains(x.ProfileParameterId)))
+                    {
+                        continue;
+                    }
+
+                    workflowConfigurations.Add(possibleWorkflowConfiguration);
+                    break;
+                }
+            }
+
+            return workflowConfigurations;
         }
 
         private Net.Profiles.Parameter GetNumberConfigurationWithChanges(NumberConfiguration apiConfiguration)
---
+++ DevPack/Storage/DOM/Model/Workflow/OrchestrationEventsSection.cs
@@ -0,0 +1,38 @@
+﻿namespace Skyline.DataMiner.Solutions.MediaOps.Plan.Storage.DOM.SlcWorkflow
+{
+    internal partial class OrchestrationEventsSection
+    {
+        private ScriptExecutionDetails scriptExecutionDetails;
+
+        internal ScriptExecutionDetails ScriptExecutionDetails
+        {
+            get
+            {
+                if (scriptExecutionDetails != null)
+                {
+                    return scriptExecutionDetails;
+                }
+
+                if (ScriptExecutionDetails.TryDeserialize(ScriptInputValues, out scriptExecutionDetails))
+                {
+                    return scriptExecutionDetails;
+                }
+
+                scriptExecutionDetails = null;
+                return scriptExecutionDetails;
+            }
+            set
+            {
+                scriptExecutionDetails = value;
+            }
+        }
+
+        internal void ApplyChanges()
+        {
+            if (scriptExecutionDetails != null)
+            {
+                ScriptInputValues = scriptExecutionDetails.Serialize();
+            }
+        }
+    }
+}
