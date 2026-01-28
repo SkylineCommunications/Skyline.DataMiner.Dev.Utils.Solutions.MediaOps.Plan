1891da130ef3497bbfd98b94cac900d74dbb6e4f
Jens Vandewalle <jens.vandewalle@skyline.be>
2026-01-28 15:14:04 +0100

    WorkflowOrchestrationSettings

---
+++ DevPack/API/Objects/Workflow/OrchestrationSettings/WorkflowCapabilitySetting.cs
@@ -0,0 +1,72 @@
+﻿namespace Skyline.DataMiner.Solutions.MediaOps.Plan.API
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using StorageWorkflow = Storage.DOM.SlcWorkflow;
+
+    internal class WorkflowCapabilitySetting : CapabilitySetting
+    {
+        internal StorageWorkflow.ProfileParameterValuesSection originalSection;
+        internal StorageWorkflow.ProfileParameterValuesSection updatedSection;
+
+        internal WorkflowCapabilitySetting(CapabilitySetting capabilitySetting) : base(capabilitySetting)
+        {
+        }
+
+        internal WorkflowCapabilitySetting(StorageWorkflow.ProfileParameterValuesSection section)
+        {
+            ParseSection(section);
+            InitTracking();
+        }
+
+        internal override Storage.DOM.DomSectionBase OriginalSection => originalSection;
+
+        internal StorageWorkflow.ProfileParameterValuesSection GetSectionWithChanges()
+        {
+            if (updatedSection == null)
+            {
+                updatedSection = IsNew ? new StorageWorkflow.ProfileParameterValuesSection() : originalSection.Clone();
+            }
+
+            updatedSection.ProfileParameterId = Id;
+            SetDiscreteValues(updatedSection, discretes);
+
+            return updatedSection;
+        }
+
+        private static IEnumerable<string> GetDiscreteValues(StorageWorkflow.ProfileParameterValuesSection section)
+        {
+            if (string.IsNullOrWhiteSpace(section.StringValue))
+            {
+                return Array.Empty<string>();
+            }
+
+            return section.StringValue.Split([";"], StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private static void SetDiscreteValues(StorageWorkflow.ProfileParameterValuesSection section, IEnumerable<string> discretes)
+        {
+            if (discretes == null || !discretes.Any())
+            {
+                section.StringValue = string.Empty;
+                return;
+            }
+
+            section.StringValue = string.Join(";", discretes);
+        }
+
+        private void ParseSection(StorageWorkflow.ProfileParameterValuesSection section)
+        {
+            originalSection = section ?? throw new ArgumentNullException(nameof(section));
+
+            Id = section.ProfileParameterId;
+            discretes.Clear();
+            foreach (var discreteValue in GetDiscreteValues(section))
+            {
+                discretes.Add(discreteValue);
+            }
+        }
+    }
+}
---
+++ DevPack/API/Objects/Workflow/OrchestrationSettings/WorkflowTextConfigurationSetting.cs
@@ -0,0 +1,45 @@
+﻿namespace Skyline.DataMiner.Solutions.MediaOps.Plan.API
+{
+    using System;
+
+    using StorageWorkflow = Storage.DOM.SlcWorkflow;
+
+    internal class WorkflowTextConfigurationSetting : TextConfigurationSetting
+    {
+        internal StorageWorkflow.ProfileParameterValuesSection originalSection;
+        internal StorageWorkflow.ProfileParameterValuesSection updatedSection;
+
+        internal WorkflowTextConfigurationSetting(TextConfigurationSetting textConfigurationSetting) : base(textConfigurationSetting)
+        {
+        }
+
+        internal WorkflowTextConfigurationSetting(StorageWorkflow.ProfileParameterValuesSection section)
+        {
+            ParseSection(section);
+            InitTracking();
+        }
+
+        internal override Storage.DOM.DomSectionBase OriginalSection => originalSection;
+
+        internal StorageWorkflow.ProfileParameterValuesSection GetSectionWithChanges()
+        {
+            if (updatedSection == null)
+            {
+                updatedSection = IsNew ? new StorageWorkflow.ProfileParameterValuesSection() : originalSection.Clone();
+            }
+
+            updatedSection.ProfileParameterId = Id;
+            updatedSection.StringValue = Value;
+
+            return updatedSection;
+        }
+
+        private void ParseSection(StorageWorkflow.ProfileParameterValuesSection section)
+        {
+            originalSection = section ?? throw new ArgumentNullException(nameof(section));
+
+            Id = section.ProfileParameterId;
+            Value = section.StringValue;
+        }
+    }
+}
