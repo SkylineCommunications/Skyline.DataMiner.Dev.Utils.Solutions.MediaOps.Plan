b955ee6629170b72935db455e8bf4d0751913ebf
Thomas <thomas.remmery@skyline.be>
2026-03-16 12:27:18 +0100

    Add maximum allowed length for discretes in the error data.

--- DevPack/Exceptions/TraceData/ResourceStudio/Capabilities/CapabilityDiscreteInvalidLengthError.cs
+++ DevPack/Exceptions/TraceData/ResourceStudio/Capabilities/CapabilityDiscreteInvalidLengthError.cs
@@ -2,6 +2,8 @@
 {
 	using System.Collections.Generic;
 
+	using Skyline.DataMiner.Solutions.MediaOps.Plan.API;
+
 	/// <summary>
 	/// Represents an error that occurs when a capability specifies one or multiple discrete values with an invalid length.
 	/// </summary>
@@ -11,5 +13,10 @@
 		/// Gets the collection of discrete values that have an invalid length.
 		/// </summary>
 		public IReadOnlyCollection<string> InvalidDiscretes { get; internal set; }
+
+		/// <summary>
+		/// Gets the maximum allowed length for discrete values.
+		/// </summary>
+		public int MaxLength { get; internal set; } = InputValidator.DefaultMaxTextLength;
 	}
 }
