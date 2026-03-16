8846ccb024b7536e491854f831d0dd5dc0162de0
Thomas <thomas.remmery@skyline.be>
2026-03-16 11:32:24 +0100

    Update logic to allow duplicate discreet values to be passed to a capability. Add hard limit to the length of a discreet option for capabilities.

---
+++ DevPack/Exceptions/TraceData/ResourceStudio/Capabilities/CapabilityDiscreteInvalidLengthError.cs
@@ -0,0 +1,15 @@
+﻿namespace Skyline.DataMiner.Solutions.MediaOps.Plan.Exceptions
+{
+	using System.Collections.Generic;
+
+	/// <summary>
+	/// Represents an error that occurs when a capability specifies one or multiple discrete values with an invalid length.
+	/// </summary>
+	public class CapabilityDiscreteInvalidLengthError : CapabilityInvalidDiscretesError
+	{
+		/// <summary>
+		/// Gets the collection of discrete values that have an invalid length.
+		/// </summary>
+		public IReadOnlyCollection<string> InvalidDiscretes { get; internal set; }
+	}
+}
