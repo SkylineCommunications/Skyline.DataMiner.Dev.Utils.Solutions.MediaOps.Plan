8308ca60f8e0189f49ec17fd3075a6d8846a455e
Thomas <thomas.remmery@skyline.be>
2026-04-08 15:59:47 +0200

    Fix build error

--- DevPack/API/Repositories/ResourceStudio/ResourcePools/ResourcePoolsRepository.cs
+++ DevPack/API/Repositories/ResourceStudio/ResourcePools/ResourcePoolsRepository.cs
@@ -542,7 +542,7 @@
 			var parentPoolsPerPool = resourcePools.ToDictionary(
 				pool => pool,
 				pool =>
-					domPools
+					(IReadOnlyCollection<ResourcePool>)domPools
 						.Where(domPool => domPool.ResourcePoolLinks
 						.Any(link => link.LinkedResourcePool.Value == pool.Id))
 						.Select(domPool =>
