886f2ab371fcf8c90423bb715b32e9654ad6c40d
Jens Vandewalle <jens.vandewalle@skyline.be>
2026-04-13 14:51:21 +0200

    first draft

---
+++ DevPack/API/Objects/Properties/Property.cs
@@ -0,0 +1,97 @@
+﻿namespace Skyline.DataMiner.Solutions.MediaOps.Plan.API
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	using StorageProperties = Storage.DOM.SlcProperties;
+
+	public abstract class Property : ApiObject
+	{
+		private StorageProperties.PropertyInstance originalInstance;
+
+		private protected Property() : base()
+		{
+			IsNew = true;
+		}
+
+		private protected Property(Guid propertyId) : base(propertyId)
+		{
+			IsNew = true;
+			HasUserDefinedId = true;
+		}
+
+		private protected Property(StorageProperties.PropertyInstance instance) : base(instance.ID.Id)
+		{
+			ParseInstance(instance);
+		}
+
+		/// <summary>
+		/// Gets or sets the name of the property.
+		/// </summary>
+		public override string Name { get; set; }
+
+		private void ParseInstance(StorageProperties.PropertyInstance instance)
+		{
+			originalInstance = instance ?? throw new ArgumentNullException(nameof(instance));
+
+			Name = instance.PropertyInfo.Name;
+		}
+	}
+
+	public class StringProperty : Property
+	{
+		public StringProperty() : base()
+		{
+		}
+		public StringProperty(Guid propertyId) : base(propertyId)
+		{
+		}
+		internal StringProperty(StorageProperties.PropertyInstance instance) : base(instance)
+		{
+			ParseInstance(instance);
+			InitTracking();
+		}
+
+		public string DefaultValue { get; set; }
+
+		public int StringSizeLimit { get; set; }
+
+		public bool IsMultipleLine { get; set; }
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				var hash = base.GetHashCode();
+				hash = (hash * 23) + (DefaultValue != null ? DefaultValue.GetHashCode() : 0);
+				hash = (hash * 23) + StringSizeLimit.GetHashCode();
+				hash = (hash * 23) + IsMultipleLine.GetHashCode();
+
+				return hash;
+			}
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj is not StringProperty other)
+			{
+				return false;
+			}
+
+			return base.Equals(other)
+				&& DefaultValue == other.DefaultValue
+				&& StringSizeLimit == other.StringSizeLimit
+				&& IsMultipleLine == other.IsMultipleLine;
+		}
+
+		private void ParseInstance(StorageProperties.PropertyInstance instance)
+		{
+			DefaultValue = instance.PropertyInfo.Default;
+			StringSizeLimit = instance.PropertyInfo.StringSizeLimit.HasValue ? (int)instance.PropertyInfo.StringSizeLimit.Value : 0;
+			IsMultipleLine = instance.PropertyInfo.IsMultiLineString.HasValue ? instance.PropertyInfo.IsMultiLineString.Value : false;
+		}
+	}
+}
---
+++ DevPack/Storage/DOM/Helpers/SlcPropertiesHelper.cs
@@ -0,0 +1,94 @@
+﻿namespace Skyline.DataMiner.Solutions.MediaOps.Plan.Storage.DOM
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	using Skyline.DataMiner.Net;
+	using Skyline.DataMiner.Net.Apps.DataMinerObjectModel;
+	using Skyline.DataMiner.Net.Messages.SLDataGateway;
+	using Skyline.DataMiner.Solutions.MediaOps.Plan.Storage.DOM.SlcProperties;
+
+	internal class SlcPropertiesHelper : DomModuleHelperBase
+	{
+		public SlcPropertiesHelper(IConnection connection) : base(SlcPropertiesIds.ModuleId, connection)
+		{
+		}
+
+		public IEnumerable<PropertyInstance> GetProperties(IEnumerable<Guid> ids)
+		{
+			if (ids == null)
+			{
+				throw new ArgumentNullException(nameof(ids));
+			}
+
+			if (!ids.Any())
+			{
+				return Enumerable.Empty<PropertyInstance>();
+			}
+
+			FilterElement<DomInstance> filter(Guid id) =>
+				DomInstanceExposers.DomDefinitionId.Equal(SlcPropertiesIds.Definitions.Property.Id)
+				.AND(DomInstanceExposers.Id.Equal(id));
+
+			return FilterQueryExecutor.RetrieveFilteredItems(
+				ids.Distinct(),
+				x => filter(x),
+				x => GetPropertyIterator(x));
+		}
+
+		public IEnumerable<PropertyInstance> GetProperties(FilterElement<DomInstance> filter)
+		{
+			if (filter == null)
+			{
+				throw new ArgumentNullException(nameof(filter));
+			}
+
+			return GetPropertyIterator(filter);
+		}
+
+		public IEnumerable<PropertyValuesInstance> GetPropertyValues(IEnumerable<Guid> ids)
+		{
+			if (ids == null)
+			{
+				throw new ArgumentNullException(nameof(ids));
+			}
+
+			if (!ids.Any())
+			{
+				return Enumerable.Empty<PropertyValuesInstance>();
+			}
+
+			FilterElement<DomInstance> filter(Guid id) =>
+				DomInstanceExposers.DomDefinitionId.Equal(SlcPropertiesIds.Definitions.PropertyValues.Id)
+				.AND(DomInstanceExposers.Id.Equal(id));
+
+			return FilterQueryExecutor.RetrieveFilteredItems(
+				ids.Distinct(),
+				x => filter(x),
+				x => GetPropertyValueIterator(x));
+		}
+
+		public IEnumerable<PropertyValuesInstance> GetPropertyValues(FilterElement<DomInstance> filter)
+		{
+			if (filter == null)
+			{
+				throw new ArgumentNullException(nameof(filter));
+			}
+
+			return GetPropertyValueIterator(filter);
+		}
+
+		private IEnumerable<PropertyInstance> GetPropertyIterator(FilterElement<DomInstance> filter)
+		{
+			return InstanceFactory.ReadAndCreateInstances(DomHelper, filter, instance => new PropertyInstance(instance));
+		}
+
+		private IEnumerable<PropertyValuesInstance> GetPropertyValueIterator(FilterElement<DomInstance> filter)
+		{
+			return InstanceFactory.ReadAndCreateInstances(DomHelper, filter, instance => new PropertyValuesInstance(instance));
+		}
+	}
+}
