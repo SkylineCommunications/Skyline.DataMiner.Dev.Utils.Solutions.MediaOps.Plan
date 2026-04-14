36f29d585c09b8d11d0d500fef720a851de8cbe9
Jens Vandewalle <jens.vandewalle@skyline.be>
2026-04-14 11:55:40 +0200

    Enhance Job model with state, key, and roll properties
    Added Key, PreRoll, PostRoll, WorkflowId (internal set), and State (JobState) properties to the Job class. Updated ToInstance and ParseInstance methods to handle these fields and their mapping to storage. Introduced the JobState enum to represent job lifecycle states. Refactored property setters for immutability where appropriate.

---
+++ DevPack/API/Objects/Workflow/Jobs/JobState.cs
@@ -0,0 +1,42 @@
+﻿namespace Skyline.DataMiner.Solutions.MediaOps.Plan.API
+{
+	/// <summary>
+	/// Represents the state of a job.
+	/// </summary>
+	public enum JobState
+	{
+		/// <summary>
+		/// The job is in draft state.
+		/// </summary>
+		Draft = 0,
+
+		/// <summary>
+		/// The job is in tentative state.
+		/// </summary>
+		Tentative = 1,
+
+		/// <summary>
+		/// The job is in confirmed state.
+		/// </summary>
+		Confirmed = 2,
+
+		/// <summary>
+		///  The job is in running state.
+		/// </summary>
+		Running = 3,
+
+		/// <summary>
+		/// The job is in completed state.
+		/// </summary>
+		Completed = 4,
+
+		//ReadyForInvoice = 5,
+
+		/// <summary>
+		/// The job is in canceled state.
+		/// </summary>
+		Canceled = 6,
+
+		//Invoiced = 7
+	}
+}
